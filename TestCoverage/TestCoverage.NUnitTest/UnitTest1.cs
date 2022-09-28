de1c73b2cd8d92e886942537accaaffb620ee379
Useless <202010014@post.au.dk>
2022-09-28 12:21:29 +0200

    removed an extra add_resultCorrect() method. which didn't seem to do anything.

--- TestCoverage/TestCoverage.NUnitTest/UnitTest1.cs
+++ TestCoverage/TestCoverage.NUnitTest/UnitTest1.cs
@@ -50,8 +50,7 @@ namespace TestCoverage.NUnitTest
             Assert.That(uut.Accumulator, Is.EqualTo(0));
         }
 
-        [Test]
-        public void Add_ResultCorrect()
+       
 
         [TestCase(1, 2, 3)]
         [TestCase(3, 3, 6)]
